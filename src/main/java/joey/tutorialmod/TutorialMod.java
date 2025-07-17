d2c153455a02896eb0feef535511a6051794a4b1
joey <joeyjrbmanzanilla@su.edu.ph>
2025-07-17 18:33:37 +0800

    important comment

---
+++ src/main/java/joey/tutorialmod/TutorialMod.java
@@ -0,0 +1,18 @@
+package joey.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+// very important comment
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+}
---
+++ src/main/java/joey/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package joey.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
